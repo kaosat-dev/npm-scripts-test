eaf54a2f96ce05d9accb104402e10efaf8974ecc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-05-24 08:44:24 +0200

    feat(doStuff): added basic api to test

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const version = require('./package.json').version
 
 console.log(`current version ${version}`)
+
+function doStuff (param = 42) {
+  return `got some param ${param}`
+}
+
+module.exports = {
+  doStuff
+}
